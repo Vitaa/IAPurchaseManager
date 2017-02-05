1903ccc70b2104a26c82cec25500f13ec0545209
Vitaliya Solomina <vita@parsable.com>
2017-02-05 12:33:20 -0800

    Carthage support

---
+++ IAPurchaseManager/IAPurchaseManager/IAPurchaseManager.h
@@ -0,0 +1,19 @@
+//
+//  IAPurchaseManager.h
+//  IAPurchaseManager
+//
+//  Created by Vitaliya Solomina on 2017-02-05.
+//  Copyright © 2017 Vitaliya Solomina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IAPurchaseManager.
+FOUNDATION_EXPORT double IAPurchaseManagerVersionNumber;
+
+//! Project version string for IAPurchaseManager.
+FOUNDATION_EXPORT const unsigned char IAPurchaseManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IAPurchaseManager/PublicHeader.h>
+
+
