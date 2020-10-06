a953572078073d410177ccacf0b72ad73f46686f
Benjamin Barnard <44757473+barnard-b@users.noreply.github.com>
2020-10-06 15:15:33 -0400

    Added Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "IAPurchaseManager",
+    platforms: [
+        .iOS(.v8)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "IAPurchaseManager",
+            targets: ["IAPurchaseManager"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "IAPurchaseManager",
+            dependencies: [],
+            path: "./",
+            exclude: [
+                "IAPurchaseManager.podspec",
+                "IAPurchaseManager/Info.plist",
+                "LICENSE",
+                "README.md"
+            ])
+    ]
+)
